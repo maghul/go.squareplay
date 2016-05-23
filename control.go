606a6fa39822597cf468e78c49f298e85ea06144
Magnus Hulthén <magnus@computer.se>
2016-05-24 00:09:44 +0200

    Decoding control using JSON message instead

--- control.go
+++ control.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"golang.org/x/text/encoding/charmap"
 )
+
+type playerJson struct {
+	Name string
+	Id   string
+}
 
 var decoder = charmap.ISO8859_1.NewDecoder()
 
@@ -33,18 +39,34 @@ func getHeader(r *http.Request, name string) string {
 	return dv
 }
 
+func (pj *playerJson) String() string {
+	return fmt.Sprintf("player:[{'Name': '%s', 'Id': '%s'}]", pj.Name, pj.Id)
+}
+
+func decodePlayers(w http.ResponseWriter, r *http.Request) []*playerJson {
+	pa := make([]*playerJson, 0)
+	d := json.NewDecoder(r.Body)
+	err := d.Decode(&pa)
+	if err != nil {
+		ilog.Println("Error decoding start request:", err)
+		w.WriteHeader(500)
+	}
+	return pa
+}
+
 func start(w http.ResponseWriter, r *http.Request) {
-	id := getHeader(r, "Airplay-Session-Id")
-	name := getHeader(r, "Airplay-Session-Name")
-	ilog.Println("Starting client: name=", name, ", id=", id)
+	ilog.Println("start client")
 
 	host := getHost(r.Host)
-	_, err := startPlayer(name, id, host)
-	if err != nil {
-		w.WriteHeader(404)
-		fmt.Fprintf(w, "Player %s[%s] could not be started: %v\r\n", name, id, err)
-	} else {
-		fmt.Fprintf(w, "Player %s[%s] started\r\n", name, id)
+	for _, pj := range decodePlayers(w, r) {
+		ilog.Println("Starting client: ", pj, " from host", host)
+		_, err := startPlayer(pj.Name, pj.Id, host)
+		if err != nil {
+			w.WriteHeader(404)
+			fmt.Fprintf(w, "Player %s[%s] could not be started: %v\r\n", pj.Name, pj.Id, err)
+		} else {
+			fmt.Fprintf(w, "Player %s[%s] started\r\n", pj.Name, pj.Id)
+		}
 	}
 }
 
