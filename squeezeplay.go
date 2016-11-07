183a47742622d0f7cd7fd6a376efb48469e336e7
Magnus Hulthén <magnus@computer.se>
2016-11-07 01:56:17 +0100

    lumberjack logging

--- squeezeplay.go
+++ squeezeplay.go
@@ -5,20 +5,32 @@ import (
 	"fmt"
 
 	"github.com/maghul/go.raopd"
+	"github.com/natefinch/lumberjack"
 	"net/http"
 	_ "net/http/pprof"
 )
 
 var apServiceRegistry *raopd.ServiceRegistry
 
+var log = raopd.GetLogger("squareplay")
+
 func main() {
-	fmt.Println("Starting SquarePlay Proxy 0.0.1(beta)")
+	l := raopd.GetLogger("")
+	l.SetLevel(raopd.LogDebug)
+	l.SetOutput(&lumberjack.Logger{
+		Filename:   "/var/log/squeezeboxserver/squareplay.log",
+		MaxSize:    500, // megabytes
+		MaxBackups: 3,
+		MaxAge:     28, //days
+	})
 
 	var port int
 	var profile int
 	flag.IntVar(&port, "w", 6111, "The server port for the proxy")
 	flag.IntVar(&profile, "pprof", 0, "Set to a port to enable profiling")
 	flag.Parse()
+
+	log.Info().Println("Starting SquarePlay Proxy 0.0.1(beta)")
 
 	if profile > 0 {
 		go http.ListenAndServe(fmt.Sprintf(":%d", profile), nil)
